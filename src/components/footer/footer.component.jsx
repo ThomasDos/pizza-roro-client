e303e8180181bb6adcdcd4535e06228c06db3467
Thomas <dosanjos.thomas@gmail.com>
2021-05-14 08:50:54 +0200

    updated footer

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,8 +1,26 @@
 import React from "react";
-import { FooterContainer } from "./footer.styles";
+import {
+  FooterContainer,
+  SocialMediaContainer,
+  CopyrightContainer,
+  LogoFacebookContainer,
+  LogoInstagramContainer,
+  LogoTwitterContainer,
+} from "./footer.styles";
 
 const Footer = () => {
-  return <FooterContainer>MON FOOTER</FooterContainer>;
+  const actualYear = new Date().getFullYear();
+  return (
+    <FooterContainer>
+      MON FOOTER
+      <SocialMediaContainer>
+        <LogoFacebookContainer />
+        <LogoInstagramContainer />
+        <LogoTwitterContainer />
+      </SocialMediaContainer>
+      <CopyrightContainer>made in {actualYear} by TDA</CopyrightContainer>
+    </FooterContainer>
+  );
 };
 
 export default Footer;
--- src/components/footer/footer.styles.jsx
+++ src/components/footer/footer.styles.jsx
@@ -1,9 +1,34 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { ReactComponent as Twitter } from "../../assets/twitter.svg";
+import { ReactComponent as Facebook } from "../../assets/facebook.svg";
+import { ReactComponent as Instagram } from "../../assets/instagram.svg";
+
+const socialMediaStyles = css`
+  width: 1.4rem;
+  height: 1.4rem;
+  margin: 0.2rem;
+`;
 
 export const FooterContainer = styled.div`
   position: fixed;
   bottom: 0;
   width: 100%;
-  height: 3rem;
+  height: 6rem;
+  padding: 1rem;
   background: lightblue;
+  color: white;
+  font-size: 0.7rem;
+`;
+
+export const SocialMediaContainer = styled.div``;
+export const CopyrightContainer = styled.div``;
+
+export const LogoFacebookContainer = styled(Facebook)`
+  ${socialMediaStyles}
+`;
+export const LogoInstagramContainer = styled(Instagram)`
+  ${socialMediaStyles}
+`;
+export const LogoTwitterContainer = styled(Twitter)`
+  ${socialMediaStyles}
 `;
